b22bc85bc7dc316a0e13d12cb56fb7174a2dce6c
Abdallah-Khalil <abdallah.khalil.nada@gmail.com>
2017-10-29 11:35:53 +0200

    first commit

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import { ApplicationModule } from './modules/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
-  await app.listen(3000);
+  await app.listen(6600);
 }
 bootstrap();
